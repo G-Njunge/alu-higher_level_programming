67782981660be1e416639af15b96c2aa23b8b0aa
G-Njunge <g.njunge@alustudent.com>
2025-06-28 01:18:47 -0700

    corrections

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-print("Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"[39:66] +
-"Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"[6] +
-"Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"[-22:-18] +
-"Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"[6] +
-"Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"[:6])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[0:6]
+print(str)
