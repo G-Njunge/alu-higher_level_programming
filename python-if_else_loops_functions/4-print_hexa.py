d8bdcb7af32c48db2505a665e3a631869ab4aeb9
G-Njunge <g.njunge@alustudent.com>
2025-07-07 00:16:36 -0700

    fourth edit

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for i in range (1,99):
-    print(f"{i} = {hex(i)}")
-
+    print("{0} = 0x{0:x}" .format(i))
--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range (0,99):
-    print(f"{i:02},", end=" ")
-print("99")
-
+    if i < 99:
+        print("{:02d}, " .format(i), end="")
+    else:
+        print("{:02d}" .format(i))
 
